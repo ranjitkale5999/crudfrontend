368e5e88d6e9172d4bf034a6cc1a03b5085afaf7
THS Interns <interns@thsystems.co.in>
2025-05-14 11:50:53 +0530

    Mutiple module and routes And Child

--- src/app/ReactiveForms/demo/demo.module.ts
+++ src/app/ReactiveForms/demo/demo.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DemoRoutingModule } from './demo-routing.module';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Demo1Component,
+    Demo2Component
+  ],
   imports: [
     CommonModule,
     DemoRoutingModule
---
+++ src/app/ReactiveForms/demo/demo2/demo2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo2',
+  standalone: false,
+  templateUrl: './demo2.component.html',
+  styleUrl: './demo2.component.css'
+})
+export class Demo2Component {
+
+}
